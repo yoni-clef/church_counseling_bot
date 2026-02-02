b85635f9fbab3bc7344b3531fe83c4c5c24b5513
yoni-clef <yoniashenafi22@gmail.com>
2026-02-02 18:09:40 +0300

    add jest.config.js for Jest testing configuration

---
+++ jest.config.js
@@ -0,0 +1,19 @@
+module.exports = {
+    preset: 'ts-jest',
+    testEnvironment: 'node',
+    roots: ['<rootDir>/src'],
+    testMatch: ['**/__tests__/**/*.ts', '**/?(*.)+(spec|test).ts'],
+    transform: {
+        '^.+\\.ts$': 'ts-jest',
+    },
+    collectCoverageFrom: [
+        'src/**/*.ts',
+        '!src/**/*.test.ts',
+        '!src/**/*.spec.ts',
+        '!src/index.ts',
+    ],
+    coverageDirectory: 'coverage',
+    coverageReporters: ['text', 'lcov', 'html'],
+    setupFilesAfterEnv: ['<rootDir>/src/test-setup.ts'],
+    testTimeout: 10000,
+};
