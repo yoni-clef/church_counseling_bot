fe16c30893465b7731e0e622da5ea18a185f3ddf
yoni-clef <yoniashenafi22@gmail.com>
2026-02-02 20:31:42 +0300

    add type definitions export for User, Counselor, Session, Message, PrayerRequest, Report, and AuditLog in the Telegram Counseling Bot

---
+++ src/types/index.ts
@@ -0,0 +1,8 @@
+// Export all type definitions
+export * from './User';
+export * from './Counselor';
+export * from './Session';
+export * from './Message';
+export * from './PrayerRequest';
+export * from './Report';
+export * from './AuditLog';
