6469ad72898964958038a0321a5f2766d6765c1e
yoni-clef <yoniashenafi22@gmail.com>
2026-02-02 20:52:52 +0300

    add index file to export all manager components in the Telegram Counseling Bot

---
+++ src/managers/index.ts
@@ -0,0 +1,8 @@
+// Export all manager components
+export { UserManager } from './UserManager';
+export { SessionManager } from './SessionManager';
+export { CounselorManager } from './CounselorManager';
+export { ReportingSystem } from './ReportingSystem';
+export { StatisticsManager } from './StatisticsManager';
+export { CleanupManager } from './CleanupManager';
+export { AuditLogManager } from './AuditLogManager';
