e6870b4cc8ae6180cc90e538e156ba776b10755d
yoni-clef <yoniashenafi22@gmail.com>
2026-02-07 17:01:37 +0300

    feat(index): ensure consistent export of all manager components including AuditLogManager and BroadcastManager

--- src/managers/index.ts
+++ src/managers/index.ts
@@ -6,3 +6,9 @@ export { ReportingSystem } from './ReportingSystem';
 export { StatisticsManager } from './StatisticsManager';
 export { CleanupManager } from './CleanupManager';
 export { AuditLogManager } from './AuditLogManager';
+
+// Export all manager components
+export * from './SessionManager';
+export * from './StatisticsManager';
+export * from './UserManager';
+export * from './BroadcastManager';
